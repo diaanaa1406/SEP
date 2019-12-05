1ce80c67a8a217a19a7b56d67bcbd3d05abb98b9
Kilgarra <baratanett.b@gmail.com>
2019-12-05 11:26:36 +0100

    1

--- Project/src/Classroom.java
+++ Project/src/Classroom.java
@@ -1,3 +1,119 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
 public class Classroom
 {
+  private String number;
+  private int numberOfSeats;
+  private boolean projector;
+  private int numberOfSockets;
+  private ArrayList<ExternalPort> externalPorts;
+  public Classroom(String number, int numberOfSeats, boolean projector, int numberOfSockets)
+  {
+    try
+    {
+      this.number=number;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+    try
+    {
+      this.numberOfSeats=numberOfSeats;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+    try
+    {
+      this.projector=projector;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+    try
+    {
+      this.numberOfSockets=numberOfSockets;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+    externalPorts=new ArrayList<>();
+  }
+
+  public void setNumber(String number)
+  {
+    try
+    {
+      this.number=number;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+  }
+
+  public void setNumberOfSeats(int numberOfSeats)
+  {
+    try
+    {
+      this.numberOfSeats=numberOfSeats;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+  }
+
+  public void setProjector(boolean projector)
+  {
+    try
+    {
+      this.projector=projector;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+  }
+
+  public void setNumberOfSockets(int numberOfSockets)
+  {
+    try
+    {
+      this.numberOfSockets=numberOfSockets;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+  }
+
+  public String getNumber()
+  {
+    return number;
+  }
+  public boolean getProjector()
+  {
+    return projector;
+  }
+
+  public int getNumberOfSeats()
+  {
+    return numberOfSeats;
+  }
+
+  public int getNumberOfSockets()
+  {
+    return numberOfSockets;
+  }
+
+  public ArrayList<ExternalPort> getExternalPorts()
+  {
+    return externalPorts;
+  }
 }
--- Project/src/ExternalPort.java
+++ Project/src/ExternalPort.java
@@ -1,3 +1,54 @@
+import java.util.InputMismatchException;
+
 public class ExternalPort
 {
+  private String type;
+  private int number;
+  public ExternalPort(String type, int number)
+  {
+    try
+    {
+      this.type=type;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+    try
+    {
+      this.number=number;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+
+  }
+  public void setType(String type)
+  {
+    try
+    {
+      this.type=type;
+
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }  }
+  public void setNumber(int number)
+  {
+    try
+    {
+      this.number=number;
+    }catch (InputMismatchException e)
+    {
+      System.out.println("Incorrect input");
+    }
+  }
+  public String getType()
+  {
+    return type;
+  }
+  public int getNumber()
+  {
+    return number;
+  }
 }
