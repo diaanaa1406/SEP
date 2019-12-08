cbfe2188b6c39a05ea3e3a9d56b71c6694e33e59
diaanaa1406 <dbocaniciu@gmail.com>
2019-12-08 19:34:36 +0100

    yes

--- Project/src/DateInterval.java
+++ Project/src/DateInterval.java
@@ -1,3 +1,28 @@
 public class DateInterval
 {
+  private MyDate startDate;
+  private MyDate endDate;
+  public DateInterval(MyDate startDate,MyDate endDate)
+  {
+    this.startDate=startDate;
+    this.endDate=endDate;
+  }
+  public void setStartDate(MyDate startDate)
+  {
+    this.startDate=startDate;
+  }
+  public void setEndDate(MyDate endDate)
+  {
+    this.endDate=endDate;
+  }
+
+  public MyDate getEndDate()
+  {
+    return endDate;
+  }
+
+  public MyDate getStartDate()
+  {
+    return startDate;
+  }
 }
